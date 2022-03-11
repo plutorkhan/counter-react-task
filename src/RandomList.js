1f22b49641d33d77b4714e0822454e97f3413e42
plutorkhan <32355469+plutorkhan@users.noreply.github.com>
2022-03-11 21:45:07 +0400

    RandomList component modified

--- src/RandomList.js
+++ src/RandomList.js
@@ -3,18 +3,18 @@ import React, { useEffect, useState } from "react";
 const RandomList = (props) => {
   const [numArr, setNumArr] = useState([]);
   useEffect(() => {
-    let arr = [];
-    for (let i = 0; i < props.randomNumber; i++) {
-      arr.push(Math.floor(Math.random() * 100));
-    }
+    let arr = [...numArr];
+    arr.push(Math.floor(Math.random() * 100));
     setNumArr(arr);
   }, [props.randomNumber]);
 
   return (
     <div>
-      {numArr.map((n) => (
-        <span>{n}</span>
-      ))}
+      <ul>
+        {numArr.map((n) => (
+          <li>{n}</li>
+        ))}
+      </ul>
     </div>
   );
 };
