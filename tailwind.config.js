7e637bad1972be518aeda9a89186a66fd44375aa
plutorkhan <32355469+plutorkhan@users.noreply.github.com>
2022-03-11 21:36:48 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      dark: "#121212",
+      lightdark: "#202020",
+      textgrey: "#aaa",
+      white: "white",
+      red: "red",
+      fikret: "orange",
+
+      // ...
+    },
+    extend: {},
+  },
+  plugins: [],
+};
